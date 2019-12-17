7e30e90e05115c5022de5b22a221563fd9e40ec8
rathokal <rathokal@partnet.cisco.com>
2019-12-17 22:29:46 +0530

    initial commit

---
+++ src/test/java/com/rajesh/spring/cloud/netflix/studnetapi/StudnetApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rajesh.spring.cloud.netflix.studnetapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudnetApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
